20d20f47dbb7101ed584e3e1a0d48b2cec8f9927
progriro <progriro@gmail.com>
2021-02-28 03:14:49 +0900

    [modified] remove useMemo

--- src/components/pages/Room.tsx
+++ src/components/pages/Room.tsx
@@ -1,59 +1,59 @@
-import React, { useCallback, useState, useRef, useEffect, useMemo } from 'react'
-import { DefaultLayouts } from '../templates'
-import { Button } from '../atoms'
-import { Box, TextField } from '@material-ui/core'
-import SendIcon from '@material-ui/icons/Send'
-import { RouteComponentProps } from 'react-router-dom'
-import Peer, { MeshRoom } from 'skyway-js'
-import { Audio } from '../atoms/Audio'
+import React, { useCallback, useState, useRef, useEffect } from "react";
+import { DefaultLayouts } from "../templates";
+import { Button } from "../atoms";
+import { Box, TextField } from "@material-ui/core";
+import SendIcon from "@material-ui/icons/Send";
+import { RouteComponentProps } from "react-router-dom";
+import Peer, { MeshRoom } from "skyway-js";
+import { Audio } from "../atoms/Audio";
 
 interface MediaStreamWithPeerId extends MediaStream {
-  peerId: string
+  peerId: string;
 }
 
 interface Props extends RouteComponentProps<{ roomId: string }> {}
 
 const Room: React.FC<Props> = (props) => {
-  const { roomId } = props.match.params
-  const roomRef = useRef<MeshRoom>(null)
+  const { roomId } = props.match.params;
+  const roomRef = useRef<MeshRoom>(null);
 
-  const [messages, setMessages] = useState<string[]>([])
-  const messageRef = useRef('')
-  const [audioMedias, setAudioMedias] = useState<MediaStreamWithPeerId[]>([])
+  const [messages, setMessages] = useState<string[]>([]);
+  const messageRef = useRef("");
+  const [audioMedias, setAudioMedias] = useState<MediaStreamWithPeerId[]>([]);
 
-  const audioTrackRef = useRef<MediaStreamTrack | null>(null)
-  const [isMuted, setIsMuted] = useState(true)
+  const audioTrackRef = useRef<MediaStreamTrack | null>(null);
+  const [isMuted, setIsMuted] = useState(true);
 
   const handleClick = useCallback(() => {
     if (roomRef.current === null) {
-      return
+      return;
     }
-    roomRef.current.send(messageRef.current)
-    setMessages([...messages, messageRef.current])
-    messageRef.current = ''
-  }, [messages])
+    roomRef.current.send(messageRef.current);
+    setMessages([...messages, messageRef.current]);
+    messageRef.current = "";
+  }, [messages]);
 
   const handleChange = useCallback(
     (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-      messageRef.current = e.target.value
+      messageRef.current = e.target.value;
     },
     []
-  )
+  );
 
   function mute() {
     if (audioTrackRef.current === null) {
-      return
+      return;
     }
-    audioTrackRef.current.enabled = false
-    setIsMuted(true)
+    audioTrackRef.current.enabled = false;
+    setIsMuted(true);
   }
 
   function unmute() {
     if (audioTrackRef.current === null) {
-      return
+      return;
     }
-    audioTrackRef.current.enabled = true
-    setIsMuted(false)
+    audioTrackRef.current.enabled = true;
+    setIsMuted(false);
   }
 
   // チャット用のEffect
@@ -61,48 +61,48 @@ const Room: React.FC<Props> = (props) => {
     const peer = new Peer({
       key: process.env.REACT_APP_SKYWAY_API_KEY,
       // debug: 3,
-    })
-    peer.on('open', () => {
+    });
+    peer.on("open", () => {
       navigator.mediaDevices
         .getUserMedia({
           audio: true,
           video: false,
         })
         .then((localStream) => {
-          localStream.getAudioTracks()[0].enabled = false
-          audioTrackRef.current = localStream.getAudioTracks()[0]
-          const room = peer.joinRoom(roomId, { stream: localStream })
+          localStream.getAudioTracks()[0].enabled = false;
+          audioTrackRef.current = localStream.getAudioTracks()[0];
+          const room = peer.joinRoom(roomId, { stream: localStream });
           // @ts-ignore
-          roomRef.current = room
-          room.on('open', () => {
-            console.log('open', room)
-            room.getLog()
-          })
-          room.on('log', (logs) => {
+          roomRef.current = room;
+          room.on("open", () => {
+            console.log("open", room);
+            room.getLog();
+          });
+          room.on("log", (logs) => {
             const chats = logs
               .map((log) => JSON.parse(log))
-              .filter((log) => log.messageType === 'ROOM_DATA')
-              .map((log) => log.message.data)
-            setMessages((prev) => [...prev, ...chats])
-          })
-          room.on('data', ({ src, data }) => {
-            console.log('data', data)
-            setMessages((prev) => [...prev, data])
-          })
-          room.on('stream', (stream) => {
-            console.log('stream', stream)
-            setAudioMedias((prev) => [...prev, stream])
-          })
+              .filter((log) => log.messageType === "ROOM_DATA")
+              .map((log) => log.message.data);
+            setMessages((prev) => [...prev, ...chats]);
+          });
+          room.on("data", ({ src, data }) => {
+            console.log("data", data);
+            setMessages((prev) => [...prev, data]);
+          });
+          room.on("stream", (stream) => {
+            console.log("stream", stream);
+            setAudioMedias((prev) => [...prev, stream]);
+          });
           // room.on('peerJoin', peerId => {})
-          room.on('peerLeave', (peerId) => {
+          room.on("peerLeave", (peerId) => {
             setAudioMedias((prev) =>
               prev.filter((media) => media.peerId !== peerId)
-            )
-          })
+            );
+          });
         })
-        .catch(console.error)
-    })
-  }, [roomId])
+        .catch(console.error);
+    });
+  }, [roomId]);
 
   return (
     <DefaultLayouts>
@@ -160,7 +160,7 @@ const Room: React.FC<Props> = (props) => {
         </Box>
       </Box>
     </DefaultLayouts>
-  )
-}
+  );
+};
 
-export default Room
+export default Room;
