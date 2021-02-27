d859e0b1a3ea9b5708b96eeeb9bfe285a58457fa
progriro <progriro@gmail.com>
2021-02-28 03:05:21 +0900

    [modified] src/components/atoms/Button.tsx

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,6 +1,26 @@
 import React from "react";
 import { Button as MaterialButton, ButtonProps } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 export const Button: React.FC<ButtonProps> = ({ children, ...rest }) => {
-  return <MaterialButton {...rest}>{children}</MaterialButton>;
+  const classes = useStyles();
+  return (
+    <MaterialButton className={classes.button} {...rest}>
+      {children}
+    </MaterialButton>
+  );
 };
+
+const useStyles = makeStyles({
+  button: {
+    borderRadius: "20px",
+    backgroundColor: "#35AD5E",
+    color: "#fff",
+    padding: "5px 20px",
+    fontWeight: "bold",
+    "&:active": {
+      backgroundColor: "#35AD5E",
+      color: "#ddd",
+    },
+  },
+});
